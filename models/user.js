493796fa76aee8d4a11bd2c7122abb6a20ba52cf
Josh Chu <joshchu96@gmail.com>
2024-06-13 16:27:51 -0700

    messagely hw

---
+++ models/user.js
@@ -0,0 +1,80 @@
+/** User class for message.ly */
+const db = require("../db");
+const ExpressError = require("../expressError");
+
+/** User of the site. */
+
+class User {
+  /** register new user -- returns
+   *    {username, password, first_name, last_name, phone}
+   */
+
+  static async register({ username, password, first_name, last_name, phone }) {
+    const result = await db.query(
+      `INSERT INTO users ( username, password, first_name, last_name, phone) VALUES ($1,$2,$3,$4) RETURNING username, password, first_name, last_name, phone`,
+      [username, password, first_name, last_name, phone]
+    );
+    return result.rows[0];
+  }
+
+  /** Authenticate: is this username/password valid? Returns boolean. */
+
+  static async authenticate(username, password, next) {
+    try {
+      if (!req.user) {
+        const err = new ExpressError("Unauthorized", 401);
+        return next(err);
+      } else {
+        if (req.user.username === username && req.user.password === password) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  /** Update last_login_at for user */
+
+  static async updateLoginTimestamp(username) {}
+
+  /** All: basic info on all users:
+   * [{username, first_name, last_name, phone}, ...] */
+
+  static async all() {}
+
+  /** Get: get user by username
+   *
+   * returns {username,
+   *          first_name,
+   *          last_name,
+   *          phone,
+   *          join_at,
+   *          last_login_at } */
+
+  static async get(username) {}
+
+  /** Return messages from this user.
+   *
+   * [{id, to_user, body, sent_at, read_at}]
+   *
+   * where to_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesFrom(username) {}
+
+  /** Return messages to this user.
+   *
+   * [{id, from_user, body, sent_at, read_at}]
+   *
+   * where from_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesTo(username) {}
+}
+
+module.exports = User;
